b8c79f95a783582ba4d60c7fb1a7ab840fdb46b3
AJ Guiles <ashiguiles@gmail.com>
2019-02-21 02:57:37 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,6 +2,7 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(int)
+<<<<<<< HEAD
 if int % 5 == 0 && int % 3 == 0
   return "FizzBuzz"
 end
@@ -13,3 +14,16 @@ if int % 5 == 0
 end
 end
 
+=======
+  if int % 3 == 0 
+    return "Fizz"
+  
+  if int % 5 == 0
+  return "Buzz"
+end
+if int % 5 == 0 && int % 3 == 0
+return "Fizzbuzz"
+end
+end
+fizzbuzz(15)
+>>>>>>> ce98c91f5d0971d32e9480497e808ee7e4b0d556
